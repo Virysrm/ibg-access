19e021dfe6902a0d8e981f41601cab6792818698
Eduardo Suárez <e.suarez.mx@gmail.com>
2021-05-28 23:34:57 -0500

    initial app module configurations added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { NgxBootstrapModule } from './externalModules/ngx-bootstrap.module';
+import { NgModule, LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common'
+import localeEs from '@angular/common/locales/es';
+
+registerLocaleData(localeEs, 'es');
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -9,10 +17,18 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    AppRoutingModule,
+    BrowserAnimationsModule,
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    HttpClientModule,
+    NgxBootstrapModule,
+    ReactiveFormsModule,
   ],
-  providers: [],
+  entryComponents: [],
+  providers: [
+    {provide: LOCALE_ID, useValue: 'es'},
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
